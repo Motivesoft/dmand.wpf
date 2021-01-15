587b1f58ed7c34f6349c6e0e5c2f8a8c335bb613
Ian Brown <18260644+ianabrown@users.noreply.github.com>
2021-01-15 16:57:24 +0100

    Rudimentary keyboard stuff

--- dmand/MainWindow.xaml.cs
+++ dmand/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO;
+using System.Threading;
 using System.Windows;
+using System.Windows.Input;
 
 namespace dmand
 {
@@ -50,6 +52,62 @@ namespace dmand
                 Console.WriteLine( $"{d.Attributes}" );
                 Contents.Items.Add( new FileInfoItem( file ) );
             }
+
+            new Thread( () => 
+            { 
+                Dispatcher.Invoke( () => {
+                    Contents.Focus();
+                } );
+            } ).Start();
+        }
+
+        private void Grid_KeyDown( object sender, KeyEventArgs e )
+        {
+            var key = e.Key == Key.System ? e.SystemKey : e.Key;
+            if ( key == Key.D && IsAltPressed )
+            {
+                Location.Focus();
+                Location.SelectAll();
+                e.Handled = true;
+            }
+            else
+            {
+                if ( key == Key.Down )
+                {
+                }
+            }
+        }
+
+        private void Grid_PreviewKeyDown( object sender, KeyEventArgs e )
+        {
+            UpdateKeyStates();
+
+            var key = e.Key == Key.System ? e.SystemKey : e.Key;
+        }
+
+        private void Grid_PreviewKeyUp( object sender, KeyEventArgs e )
+        {
+            UpdateKeyStates();
+        }
+
+        void UpdateKeyStates()
+        {
+            IsCtrlPressed = ( Keyboard.IsKeyDown( Key.LeftCtrl ) || Keyboard.IsKeyDown( Key.RightCtrl ) );
+            IsAltPressed = ( Keyboard.IsKeyDown( Key.LeftAlt ) || Keyboard.IsKeyDown( Key.RightAlt ) );
+            IsShiftPressed = ( Keyboard.IsKeyDown( Key.LeftShift ) || Keyboard.IsKeyDown( Key.RightShift ) );
+        }
+
+        private bool IsAltPressed
+        {
+            get; set;
+        }
+        private bool IsCtrlPressed
+        {
+            get; set;
+        }
+        private bool IsShiftPressed
+        {
+            get; set;
         }
     }
 }
